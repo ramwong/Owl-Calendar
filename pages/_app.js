e10507443f1eeca7be6dc41c250cd682b3ef2045
ramwong <ramwong@users.noreply.github.com>
2022-04-22 09:58:35 +0000

    deploy: 5ba09efd9c2a86bc5a47c9b8dfb2214e97c4274a

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "./Calendar.css";
-import "./Clock.css";
-import "./DateTimePicker.css"
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
